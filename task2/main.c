577599ff97a3becf9a1d37bb460b6a2e3eb5064b
rasalrai <jula.stachowiak@gmail.com>
2020-02-11 21:09:06 +0100

    started task 2

---
+++ task2/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/sem.h>
+#include <sys/msg.h>
+
+#include <time.h>
+#include <string.h>
+
+
+int main() {
+    
+}
