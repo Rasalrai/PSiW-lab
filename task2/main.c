4a03bf33520c6a6a9d5fd631516b6e2c601fa68e
rasalrai <jula.stachowiak@gmail.com>
2020-02-20 22:52:00 +0100

    starting 2nd task's idea

--- task2/main.c
+++ task2/main.c
@@ -1,18 +1,29 @@
+// ZADANIE 2: Swiety Mikolaj
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <sys/ipc.h>
-#include <sys/shm.h>
-#include <sys/sem.h>
-#include <sys/msg.h>
+#include <pthread.h>
 
 #include <time.h>
 #include <string.h>
 
 
 int main() {
-        
+
+
 }
+
+
+/*
+    9 reindeers
+    3/10 elves
+
+    two additional threads for handling elves and reindeers
+
+    the handlers would send signals to Santa Claus, who'd have custom reactions to them
+
+*/
