365b110d92f5547f6721047bf57f3dab09427680
Julia <jula.stachowiak@gmail.com>
2020-02-23 19:02:27 +0100

    how to make water

--- task2/water.c
+++ task2/water.c
@@ -33,7 +33,7 @@ void o_producer(unsigned int seed) {
 }
 
 void water_assembler() {
-    // wait for H mutex and O mutex
+    // lower H mutex and O mutex
 }
 
 // ############################################################
@@ -45,31 +45,14 @@ void water_assembler() {
 int main() {
     // init stuff
     unsigned int global_seed = time(NULL), local_seed;
-    *elf_cond = *reindeer_cond = PTHREAD_COND_INITIALIZER;
-
-    // create Santa thread
-    local_seed = rand_r(&global_seed);
-    pthread_create(santa_t, NULL, santa_claus, local_seed);
-
-    // create reindeers and elves
-    for(int i=0;i<reindeersN;i++) {
-        local_seed = rand_r(&global_seed);
-        pthread_create(reindeer_t + i, NULL, reindeer, local_seed);
-    }
-
-    for(int i=0;i<elvesN;i++) {
-        local_seed = rand_r(&global_seed);
-        pthread_create(elf_t + i, NULL, elf, local_seed);
-    }
+    
+    
 }
 
 
 /*
-    9 reindeers
-    3/10 elves
-
-    two additional threads for handling elves and reindeers
-
-    the handlers would send signals to Santa Claus, who'd have custom reactions to them
-
+    H Hydrogen producers, O Oxygen producers
+    2 processes for couting incoming molecules (of either type)
+    2 processes for managing mutexes (?)
+    water assembler: locks H and O counters, decreases them and outputs a water molecule
 */
